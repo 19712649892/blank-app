d02b781c835a428ad171f302784de1f59f06d410
19712649892 <3112868493@qq.com>
2026-04-11 11:20:17 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,10 +18,9 @@ def main():
     # Once we have the dependencies, add a selector for the app mode on the sidebar.
     st.sidebar.title("请选择以下选项")
     app_mode = st.sidebar.selectbox("",
-        ["网页说明", "程序运行", "代码展示"])
+        ["网页说明", "程序运行"])
     if app_mode == "网页说明":
         st.sidebar.success('请选择“程序运行”开始体验')
-
     elif app_mode == "程序运行":
         readme_text.empty()
         run_the_app()
